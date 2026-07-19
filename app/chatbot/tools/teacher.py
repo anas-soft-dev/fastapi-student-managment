ce39c312478576e777b8713cfc3e212c7efdd001
Syed-Mohsin-Ali786 <syedmohsinali4345@gmail.com>
2026-07-19 16:18:04 +0500

    student and teacher crude operation using chatbot

--- app/chatbot/tools/teacher.py
+++ app/chatbot/tools/teacher.py
@@ -1,16 +1,103 @@
 from langchain.tools import tool
 from app.models.user import User
 from app.models.role import Role
+from app.schemas.user import UserRegisterSchema
+from app.schemas.chat import AddUserInput, DeleteUser, UpdateUser
+from typing import Optional
+from pydantic import EmailStr
+from sqlalchemy import or_
 
 
 def make_teacher_tools(db):
-    def _format_teacher(user: User)->str:
+    def _teacher_query():
+        return db.query(User).join(User.roles).filter(Role.name == "teacher")
+
+    def _format_teacher(user: User) -> str:
         return f"id= {user.id}, name= {user.name}, email= {user.email}"
 
+    def _find_user(email, user_id) -> dict:
+        user = (
+            db.query(User).filter(or_(User.email == email, User.id == user_id)).first()
+        )
+        return user
+
     @tool
-    def list_teacher()->str:
+    def list_teachers() -> str:
         """list all teachers with their id, name and email"""
-        users = db.query(User).join(User.roles).filter(Role.name == "teacher").all()
+        users = _teacher_query().all()
         return "\n".join(_format_teacher(user) for user in users)
-    
-    return [list_teacher]
+
+    @tool
+    def search_teacher(query: str) -> str:
+        """
+        Find teachers whose name or email contains the given text.
+        """
+        users = (
+            _teacher_query()
+            .filter(or_(User.name.contains(query), User.email.contains(query)))
+            .all()
+        )
+
+        return "\n".join(_format_teacher(user) for user in users)
+
+    @tool(args_schema=AddUserInput)
+    def add_teacher(name: str, email: str, password: str) -> str:
+        """
+        To add teacher in database use this tool
+        """
+        existing_user = db.query(User).where(User.email == email).first()
+        if existing_user:
+            return f"{email} already exist"
+        user = User(name=name, email=email, password=password)
+        role = db.query(Role).where(Role.name == "teacher").first()
+        user.assign_role(role)
+
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        output = f"{user.name} is successfully added"
+        return output
+
+    @tool(args_schema=DeleteUser)
+    def delete_teacher(
+        email: Optional[EmailStr] = None, user_id: Optional[str] = None
+    ) -> str:
+        """
+        Delete teacher whose email or id matches the given value.
+        Provide either email or user_id.
+        """
+
+        existing_teacher = _find_user(email=email, user_id=user_id)
+        if not existing_teacher:
+            return "No teacher exist"
+        db.delete(existing_teacher)
+        db.commit()
+        output = f"{existing_teacher.email} is successfully delete"
+        return output
+
+    @tool(args_schema=UpdateUser)
+    def update_teacher(
+        email: Optional[EmailStr] = None,
+        user_id: Optional[str] = None,
+        name: Optional[str] = None,
+        new_email: Optional[EmailStr] = None,
+        password: Optional[str] = None,
+    ):
+        """Update a teacher's information. Provide email or teacher_id to find the teacher, plus fields to update."""
+        existing_teacher = _find_user(email=email, user_id=user_id)
+        if not existing_teacher:
+            return "No teacher exist"
+
+        if name:
+            existing_teacher.name = name
+        if new_email:
+            existing_teacher.email = new_email
+        if password:
+            existing_teacher.password = password
+
+        db.commit()
+        db.refresh(existing_teacher)
+        output = f"{existing_teacher.email} is successfully update"
+        return output
+
+    return [list_teachers, search_teacher, add_teacher, delete_teacher, update_teacher]
--- app/schemas/chat.py
+++ app/schemas/chat.py
@@ -1,9 +1,37 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator,EmailStr
+from app.auth import make_hash
+from typing import Optional
+
 
 class ChatInput(BaseModel):
     message: str
     conversation_id: str
 
+
 class ChatOutput(BaseModel):
     reply: str
     conversation_id: str
+
+
+class AddUserInput(BaseModel):
+    name: str = Field(description="The user's full name")
+    email: str = Field(description="The user's email address")
+    password: str = Field(description="The user's password")
+
+    @field_validator("password")
+    def hash_password(cls, value) -> str:
+        return make_hash(value)
+
+class DeleteUser(BaseModel):
+    email:Optional[EmailStr]=Field(default=None,description="The user's email address")
+    user_id:Optional[str]=Field(default=None, description="ID of user")
+    
+class UpdateUser(BaseModel):
+    email: Optional[EmailStr] = Field(None, description="Current email to find the user")
+    user_id: Optional[str] = Field(None, description="Current ID to find the user")
+    name: Optional[str] = Field(None, description="New name to update")
+    new_email: Optional[EmailStr] = Field(None, description="New email to update")
+    password: Optional[str] = Field(None, description="New password to update")
+    @field_validator("password")
+    def hash_password(cls, value) -> str:
+        return make_hash(value)
