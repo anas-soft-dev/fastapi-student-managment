1bd1ee143e87a7aa6bf19e2eac3277cdacb79ed0
ANAS <anas170645@gmail.com>
2026-07-21 13:33:02 +0500

    all origin allowed

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ origins = [o.strip() for o in os.getenv("ALLOWED_ORIGINS", default_origins).spli
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
